251ede1c05b2e631aada62433da36647beabfbd3
justin <justinmvail@gmail.com>
2018-08-06 07:55:47 -0400

    Adding test coverage for TimeZoneRectifier

---
+++ src/test/java/realestateScraper/translation/SimpleTimeZoneRectifierTest.java
@@ -0,0 +1,41 @@
+package realestateScraper.translation;
+
+import org.junit.Assert;
+import org.junit.Test;
+import realestateScraper.DomainObjects.County;
+import realestateScraper.DomainObjects.TimeZone;
+
+import java.time.LocalTime;
+
+public class SimpleTimeZoneRectifierTest {
+
+    @Test
+    public void rectifyTimeZoneFromCentralToEastern() {
+        LocalTime centralTime = LocalTime.now();
+        LocalTime easternTime = SimpleTimeZoneRectifier.rectifyTimeZone(centralTime, County.BAY, TimeZone.ET);
+        Assert.assertEquals(centralTime, easternTime.minusHours(1));
+    }
+
+    @Test
+    public void rectifyTimeZoneFromEasternToCentral() {
+        LocalTime easternTime = LocalTime.now();
+        LocalTime centralTime = SimpleTimeZoneRectifier.rectifyTimeZone(easternTime, County.BREVARD, TimeZone.CT);
+        Assert.assertEquals(easternTime, centralTime.minusHours(1));
+    }
+
+    @Test
+    public void rectifyTimeZoneFromCentralToCentral() {
+        LocalTime centralTime = LocalTime.now();
+        LocalTime rectifiedTime = SimpleTimeZoneRectifier.rectifyTimeZone(centralTime, County.BAY, TimeZone.CT);
+        Assert.assertEquals(centralTime, rectifiedTime);
+    }
+
+    @Test
+    public void rectifyTimeZoneFromEasterToEastern() {
+        LocalTime easternTime = LocalTime.now();
+        LocalTime rectifiedTime = SimpleTimeZoneRectifier.rectifyTimeZone(easternTime, County.BREVARD, TimeZone.ET);
+        Assert.assertEquals(easternTime, rectifiedTime);
+    }
+
+
+}
