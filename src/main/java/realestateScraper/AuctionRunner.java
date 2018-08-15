a02df08f7932008f5aa17b5e363d0c0b8f566cbb
justi <justinmvail@gmail.com>
2018-08-15 17:34:57 -0400

    Refactored AuctionRunner to be more readable and updatable

--- src/main/java/realestateScraper/AuctionRunner.java
+++ src/main/java/realestateScraper/AuctionRunner.java
@@ -13,28 +13,41 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class AuctionRunner {
+    private static TaxAuctionService taxAuctionService = new RealTaxDeedScraper(false);
+    private static FileExporter fileExporter = new GoogleCalendarCSVExporter();
+
     public static void main( String[] args ) throws IOException, InterruptedException {
+        long startTime = System.nanoTime();
         String exportFileLocation = args[0];
-        List<Auction> allAuctions = new ArrayList<>();
-        TaxAuctionService taxAuctionService = new RealTaxDeedScraper(false);
+        String strDate = args[1];
         County[] allCounties = County.class.getEnumConstants();
+        List<Auction> allAuctions = getAllAuctions(allCounties,  LocalDate.parse(strDate), taxAuctionService);
+        populateAllAuctionListings(allAuctions, taxAuctionService);
+        AuctionTimeUpdater.updateAuctionTimesForTimeZone(allAuctions, TimeZone.ET);
+        fileExporter.export(exportFileLocation, allAuctions);
+        long endTime = System.nanoTime();
+        long duration = (endTime - startTime)/1000000000;
+        System.out.println("Total duration: "+ duration);
+        System.out.println("Good Bye.");
+    }
+
+    private static List<Auction> getAllAuctions(County[] allCounties, LocalDate date, TaxAuctionService taxAuctionService) throws IOException, InterruptedException {
+        List<Auction> allAuctions = new ArrayList<>();
         for(County county : allCounties){
             System.out.print("Getting Auctions for "+county.getCountyName()+"...   ");
-            List<Auction> auctionList = taxAuctionService.getAllAuctionDatesByMonth(AuctionType.TAXDEED, county, LocalDate.parse("2018-08-01"));
-            allAuctions.addAll(auctionList);
+            List<Auction> countyAuctionList = taxAuctionService.getAllAuctionDatesByMonth(AuctionType.TAXDEED, county, date);
+            allAuctions.addAll(countyAuctionList);
             System.out.println("complete." );
         }
+        return allAuctions;
+    }
 
+    private static void populateAllAuctionListings(List<Auction> allAuctions, TaxAuctionService taxAuctionService) throws IOException {
         for(Auction auction : allAuctions){
             System.out.print("Getting listings for "+auction.getCounty().getCountyName()+" auction on "+auction.getDate()+"...   ");
             List<AuctionListing> auctionListings = taxAuctionService.getAuctionListings(auction);
             auction.setAuctionListings(auctionListings);
             System.out.println("complete");
         }
-
-        //Update TimeZones and save.
-        AuctionTimeUpdater.updateAuctionTimesByTimeZone(allAuctions, TimeZone.ET);
-        FileExporter fileExporter = new GoogleCalendarCSVExporter();
-        fileExporter.export(exportFileLocation, allAuctions);
     }
 }
--- src/main/java/realestateScraper/translation/AuctionTimeUpdater.java
+++ src/main/java/realestateScraper/translation/AuctionTimeUpdater.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 public class AuctionTimeUpdater {
 
-    public static void updateAuctionTimesByTimeZone(List<Auction> auctions, TimeZone timeZone){
+    public static void updateAuctionTimesForTimeZone(List<Auction> auctions, TimeZone timeZone){
         for (Auction auction : auctions){
             SimpleTimeZoneRectifier.rectifyTimeZone(auction.getTime(), auction.getCounty(), timeZone);
         }
