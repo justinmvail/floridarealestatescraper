6a1dbcf61a720180b12ee66b312e51a5c5ef49a9
justi <justinmvail@gmail.com>
2018-08-15 19:51:23 -0400

    looking into multithreading... this changeset is incomplete.  I need to switch computers now though...

--- src/main/java/realestateScraper/AuctionRunner.java
+++ src/main/java/realestateScraper/AuctionRunner.java
@@ -11,20 +11,22 @@ import java.io.IOException;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class AuctionRunner {
     private static TaxAuctionService taxAuctionService = new RealTaxDeedScraper(false);
-    private static FileExporter fileExporter = new GoogleCalendarCSVExporter();
+    private static FileExporter emailfileExporter = new GoogleCalendarCSVExporter();
 
     public static void main( String[] args ) throws IOException, InterruptedException {
         long startTime = System.nanoTime();
-        String exportFileLocation = args[0];
+        String googleCalendarExportFileLocation = args[0];
         String strDate = args[1];
         County[] allCounties = County.class.getEnumConstants();
         List<Auction> allAuctions = getAllAuctions(allCounties,  LocalDate.parse(strDate), taxAuctionService);
         populateAllAuctionListings(allAuctions, taxAuctionService);
         AuctionTimeUpdater.updateAuctionTimesForTimeZone(allAuctions, TimeZone.ET);
-        fileExporter.export(exportFileLocation, allAuctions);
+        emailfileExporter.export(googleCalendarExportFileLocation, allAuctions);
         long endTime = System.nanoTime();
         long duration = (endTime - startTime)/1000000000;
         System.out.println("Total duration: "+ duration);
@@ -43,11 +45,21 @@ public class AuctionRunner {
     }
 
     private static void populateAllAuctionListings(List<Auction> allAuctions, TaxAuctionService taxAuctionService) throws IOException {
+        ExecutorService executor = Executors.newFixedThreadPool(10);
+
         for(Auction auction : allAuctions){
-            System.out.print("Getting listings for "+auction.getCounty().getCountyName()+" auction on "+auction.getDate()+"...   ");
-            List<AuctionListing> auctionListings = taxAuctionService.getAuctionListings(auction);
-            auction.setAuctionListings(auctionListings);
-            System.out.println("complete");
+            Runnable runnableTask = () -> {
+                System.out.print("Getting listings for "+auction.getCounty().getCountyName()+" auction on "+auction.getDate()+"...   ");
+                List<AuctionListing> auctionListings = null;
+                try {
+                    auctionListings = taxAuctionService.getAuctionListings(auction);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                auction.setAuctionListings(auctionListings);
+                System.out.println("complete");
+            };
+            executor.submit(runnableTask);
         }
     }
 }
