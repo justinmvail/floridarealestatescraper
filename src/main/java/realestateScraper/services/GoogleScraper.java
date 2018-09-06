7c265387aa17ed15cc0491827aa7b253a905daee
justi <justinmvail@gmail.com>
2018-09-06 17:31:34 -0400

    updated GoogleScraper to add a new button to the form and submit that way not sure why this still work and the old way doesn't

--- src/main/java/realestateScraper/services/GoogleScraper.java
+++ src/main/java/realestateScraper/services/GoogleScraper.java
@@ -2,9 +2,10 @@ package realestateScraper.services;
 
 import com.gargoylesoftware.htmlunit.BrowserVersion;
 import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlElement;
+import com.gargoylesoftware.htmlunit.html.HtmlForm;
 import com.gargoylesoftware.htmlunit.html.HtmlInput;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
-import com.gargoylesoftware.htmlunit.html.HtmlSubmitInput;
 
 import java.io.IOException;
 
@@ -15,16 +16,34 @@ public class GoogleScraper extends HtmlUnitScraper implements SearchEngineResult
         HtmlPage googleHomePage = webClient.getPage("http://www.google.com");
         HtmlInput searchInput = googleHomePage.getElementByName("q");
         searchInput.setValueAttribute(searchPhrase);
-        HtmlSubmitInput submitSearchButton = googleHomePage.getElementByName("btnK");
-        HtmlPage googleResultsPage = submitSearchButton.click();
+        HtmlForm form = googleHomePage.getElementByName("f");
+        HtmlPage googleResultsPage = addButtonToFormAndSubmit(googleHomePage, form);
         String strUrl = googleResultsPage.getUrl().toString();
         webClient.close();
         return strUrl;
     }
 
+    private HtmlPage addButtonToFormAndSubmit(HtmlPage calendarPage, HtmlForm form) throws IOException {
+        HtmlElement button = (HtmlElement) calendarPage.createElement("button");
+        button.setAttribute("type", "submit");
+        form.appendChild(button);
+        return button.click();
+    }
+
     private WebClient getGoogleWebClient(boolean useLogging){
-        final WebClient webClient = new WebClient(BrowserVersion.FIREFOX_52);
+        final WebClient webClient = new WebClient(BrowserVersion.CHROME);
         setScraperLogging(webClient, useLogging);
         return webClient;
+    }
+
+    public static void main(String[] args){
+        GoogleScraper googleScraper = new GoogleScraper();
+        String url = null;
+        try {
+            url = googleScraper.getUrlForSearchResults("233 SUNSET BOULEVARD E PUNTA GORDA, FL- 33982");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println(url);
     }
 }
