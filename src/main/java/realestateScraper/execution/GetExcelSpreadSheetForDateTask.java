9ad46d7e3c7c1c8b00a791a1a06fc0f6797490b0
justi <justinmvail@gmail.com>
2018-09-29 14:46:16 -0400

    Check validity of URL before using them in excel export.  Also update sheet names.

--- src/main/java/realestateScraper/execution/GetExcelSpreadSheetForDateTask.java
+++ src/main/java/realestateScraper/execution/GetExcelSpreadSheetForDateTask.java
@@ -15,36 +15,110 @@ import realestateScraper.translation.AuctionTimeUpdater;
 
 import java.io.IOException;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.concurrent.ExecutionException;
 
 public class GetExcelSpreadSheetForDateTask extends AbstractParentTask {
 
     public static void main( String[] args ) throws IOException, InterruptedException, ExecutionException {
+
+        startTiming();
+
         final TaxAuctionService taxAuctionService = new RealTaxDeedScraper(false);
         final MlsService mlsService = new ZillowScraper(false);
         final SearchEngineResultService searchEngineResultService = new GoogleScraper();
         final FileExporter fileExporter = new XLSXFileExporter();
 
-        startTiming();
+        String strStartDate = args[0];
+        String strStopDate = args[1];
+        float minimumValue = Float.parseFloat(args[2]);
+        int numberOfThreads = Integer.parseInt(args[3]);
+        String exportPath = args[4];
 
-        String strDate = args[0];
-        int numberOfThreads = Integer.parseInt(args[1]);
-        String exportPath = args[2];
-
+        List<LocalDate> dates =  getDatesInRange(LocalDate.parse(strStartDate), LocalDate.parse(strStopDate));
         County[] allCounties = County.class.getEnumConstants();
-        List<Auction> auctions = getAllAuctionsByDate(allCounties, LocalDate.parse(strDate), taxAuctionService, numberOfThreads);
+        List<Auction> auctions = new ArrayList<>();
+
+        for(LocalDate date : dates){
+            auctions.addAll(getAllAuctionsByDate(allCounties, date, taxAuctionService, numberOfThreads));
+        }
+
         populateAllAuctionListings(auctions, taxAuctionService, numberOfThreads);
         AuctionTimeUpdater.updateAuctionTimesForTimeZone(auctions, TimeZone.ET);
         //We only want to use 2 threads for Zillow.  They get mad at robots.
         populateAllMlsListings(auctions, mlsService, 2);
         populateAllSearchEngineResults(auctions, searchEngineResultService, numberOfThreads);
         auctions.removeIf(auction -> auction.getAuctionListings()==null);
-        auctions.sort(Comparator.comparing(Auction::getTime));
-        auctions.forEach(auction -> auction.getAuctionListings().sort(Comparator.comparing(AuctionListing::getAssessedValue)));
-        fileExporter.export(exportPath+strDate+".xlsx", auctions);
+        auctions.sort(getViableListingComparator(minimumValue));
+        auctions.forEach(auction -> auction.getAuctionListings().sort(getAssessedValueComparator()));
+        fileExporter.export(getExportFilePath(exportPath, strStartDate, strStopDate), auctions);
         stopTiming();
         System.out.println("Good Bye.");
+    }
+
+    private static List<LocalDate> getDatesInRange(LocalDate startDate, LocalDate endDate){
+        List<LocalDate> dates = new ArrayList<>();
+        LocalDate nextDate = startDate;
+        while(nextDate.isBefore(endDate) || nextDate.isEqual(endDate)){
+            dates.add(nextDate);
+            nextDate = nextDate.plusDays(1);
+        }
+        return dates;
+    }
+
+    private static String getExportFilePath(String exportPath, String strStartDate, String strStopDate){
+        return   exportPath
+                +strStartDate
+                +" to "
+                +strStopDate
+                +" -ran on- "
+                +LocalDateTime.now()
+                .toString()
+                .substring(0, 16)
+                .replace(":","")
+                .replace("T"," at ")
+                +".xlsx";
+    }
+
+    private static Comparator<Auction> getViableListingComparator(float minimumValue){
+        return (auctionOne, auctionTwo) -> {
+            int auctionOneViableListingCount = getCountOfViableAuctions(auctionOne, minimumValue);
+            int auctionTwoViableListingCount = getCountOfViableAuctions(auctionTwo, minimumValue);
+            return auctionTwoViableListingCount-auctionOneViableListingCount;
+        };
+    }
+
+    private static int getCountOfViableAuctions(Auction auction, float minimumValue){
+        Set<AuctionListing> viableAuctionListings = new HashSet<>();
+        for (AuctionListing auctionListing : auction.getAuctionListings()){
+            MlsListing mlsListing = auctionListing.getMlsListing();
+            if(mlsListing != null && mlsListing.getPriceEstimate() != null && mlsListing.getPriceEstimate()>=minimumValue){
+                viableAuctionListings.add(auctionListing);
+            }
+        }
+        return viableAuctionListings.size();
+    }
+
+    private static Comparator<AuctionListing> getAssessedValueComparator(){
+        return (auctionListingOne, auctionListingTwo) -> {
+            if(auctionListingOne.getAssessedValue() == null && auctionListingTwo.getAssessedValue() == null){
+                return 0;
+            }if(auctionListingOne.getAssessedValue() == null){
+                return -1;
+            }if(auctionListingTwo.getAssessedValue() == null){
+                return 1;
+            }else if(auctionListingOne.getAssessedValue() < auctionListingTwo.getAssessedValue()){
+                return -1;
+            }else if(auctionListingTwo.getAssessedValue() < auctionListingOne.getAssessedValue()){
+                return 1;
+            }else{
+                return 0;
+            }
+        };
     }
 }
